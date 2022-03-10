b091fe3c04a19cc4445798365fbe65fec14f4419
Jesse Terblanche <jessenterblanche@gmail.com>
2022-03-10 12:35:16 +0000

    test

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,7 @@
 const mystorage = window.localStorage
 
-function login(){
-    fetch(`https://clonebackend.herokuapp.com/user/`, {
+async function login(){
+    const response = await fetch(`https://clonebackend.herokuapp.com/user/`, {
         method:'PATCH',
         body: JSON.stringify({
             "email": document.querySelector('#semail').value,
@@ -11,18 +11,30 @@ function login(){
             'Content-type': 'application/json',
         }
     })
-    .then(res => res.json())
-    .then(data => {
-        console.log(data);
-        // if data return is null if statement later
-        if(data.data == null){
-            alert('user doesnt exist')
-        }else{
-            alert('Login successful!')
-            mystorage.setItem('userdetails', JSON.stringify(data.data))
-            window.location.href = './home.html'
-        }
-    })
+
+    const data = await response.json()
+    if(data.data == null){
+        alert('user doesnt exist')
+    }else{
+        alert('Login successful!')
+        mystorage.setItem('userdetails', JSON.stringify(data.data))
+        window.location.href = './home.html'
+    }
+
+
+
+    // .then(res => res.json())
+    // .then(data => {
+    //     console.log(data);
+    //     // if data return is null if statement later
+        // if(data.data == null){
+        //     alert('user doesnt exist')
+        // }else{
+        //     alert('Login successful!')
+        //     mystorage.setItem('userdetails', JSON.stringify(data.data))
+        //     window.location.href = './home.html'
+        // }
+    // })
 }
 
 function register(){
